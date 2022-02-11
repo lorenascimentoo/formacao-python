eabc2cd8933d2e3c5a1cc85ceeed641b1b89cdea
Lorena Nascimento <lorena.nascimento28@gmail.com>
2022-02-11 10:05:41 -0300

    Aula 4 - Curso de Python 3 PARTE 1

--- python-fundamentos/adivinhacao.py
+++ python-fundamentos/adivinhacao.py
@@ -3,23 +3,28 @@ print("Bem vindo ao jogo de adivinhação!")
 print("***********************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute_str = input("Digite o seu numero: ")
+while(rodada <= total_de_tentativas):
+    #interpolação de string
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
 
-print("Você digitou: ", chute_str)
+    chute_str = input("Digite o seu numero: ")
+    print("Você digitou: ", chute_str)
+    chute = int(chute_str)
 
-chute = int(chute_str)
+    acertou = chute == numero_secreto
+    maior = chute>numero_secreto
+    menor = chute<numero_secreto
 
-acertou = chute == numero_secreto
-maior = chute>numero_secreto
-menor = chute<numero_secreto
-
-if(acertou):
-    print("Você acertou!")
-else:
-    if(maior):
-        print("Você errou! O seu chute foi maior que o número secreto.")
-    #elif = o else if
-    elif(menor):
-        print("Você errou! O seu chute foi menor que o número secreto.")
+    if(acertou):
+        print("Você acertou!")
+    else:
+        if(maior):
+            print("Você errou! O seu chute foi maior que o número secreto.")
+        #elif = o else if
+        elif(menor):
+            print("Você errou! O seu chute foi menor que o número secreto.")
+    rodada = rodada + 1
 print("Fim do jogo")
